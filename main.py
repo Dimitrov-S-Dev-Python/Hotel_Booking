e801e701b3a5cc46538f31ed308fad8abd86c160
Stan <dimitrov.s.dev@gmail.com>
2023-04-20 21:17:19 +0300

    Adding the logic

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
+import pandas
+
+df = pandas.read_csv("hotels.csv")
+
 
 class Hotel:
+
+    def __init__(self, id):
+        pass
+
     def book(self):
+        pass
+
+    def available(self):
         pass
 
 
 class Conformation:
+
+    def __init__(self, customer_name, hotel):
+        pass
+
     def generate(self):
         pass
+
+
+print(df)
+id = input("Enter the id of the Hotel")
+hotel = Hotel(id)
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = Conformation(hotel)
+    reservation_ticket.generate()
+else:
+    print("Hotel is not free")
+
