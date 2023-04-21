24131919c69dbec510c00a76121220d6847108a1
Stan <dimitrov.s.dev@gmail.com>
2023-04-21 08:40:51 +0300

    Checking the availability and switch from yes to no

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import pandas
 
-df = pandas.read_csv("hotels.csv")
+df = pandas.read_csv("hotels.csv", dtype={"id": str})
 
 
 class Hotel:
-
-    def __init__(self, id):
-        pass
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
 
     def book(self):
-        pass
+        """Books a hotel by changing its availability from yes to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
 
     def available(self):
-        pass
+        """Check if the Hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
 
 
 class Conformation:
@@ -25,8 +31,8 @@ class Conformation:
 
 
 print(df)
-id = input("Enter the id of the Hotel")
-hotel = Hotel(id)
+hotel_ID = input("Enter the id of the Hotel")
+hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
     name = input("Enter your name: ")
