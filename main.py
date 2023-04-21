dcad6a7675e0559797680293347aacff0630600d
Stan <dimitrov.s.dev@gmail.com>
2023-04-21 08:51:48 +0300

    Making the reservation

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ df = pandas.read_csv("hotels.csv", dtype={"id": str})
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
 
     def book(self):
         """Books a hotel by changing its availability from yes to no"""
@@ -23,11 +24,18 @@ class Hotel:
 
 class Conformation:
 
-    def __init__(self, customer_name, hotel):
-        pass
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
 
     def generate(self):
-        pass
+        content = f"""
+               Thank you for your reservation!
+               Here are your booking data:
+               Name: {self.customer_name}
+               Hotel name: {self.hotel.name}
+               """
+        return content
 
 
 print(df)
@@ -36,7 +44,7 @@ hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
     name = input("Enter your name: ")
-    reservation_ticket = Conformation(hotel)
+    reservation_ticket = Conformation(customer_name=name, hotel_object=hotel)
     reservation_ticket.generate()
 else:
     print("Hotel is not free")
